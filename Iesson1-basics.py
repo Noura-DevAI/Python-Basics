27d528a309cc48815b6d36973b3b4f60798eb683
Noura-DevAI <nourhsatam4@gmail.com>
2026-09-17 17:11:54 +0300

    Add exercises for data types and rectangle area

---
+++ Iesson1-basics.py
@@ -0,0 +1,16 @@
+# Exercise 1: Data Types & Conversion
+x = "10"
+x = int(x)
+y = 5
+result = x + y
+print("Result=", result)
+print(type(x))
+
+# Exercise 2: Rectangle Area Calculator
+length = int(input("Enter the length :"))
+width = int(input("Enter the width:"))
+
+area = length * width
+
+print("Area=", area)
+print(type(length))
